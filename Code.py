2682027bb7292982a95ca03a7b25ae6048d33c6f
Rishabh920 <72196212+Rishabh920@users.noreply.github.com>
2020-10-01 16:34:23 +0530

    Create Code.py

---
+++ Code.py
@@ -0,0 +1,14 @@
+def newFile():
+    global file
+    root.title("Untitled - Notepad")
+    file = None
+    TextArea.delete(1.0, END)
+
+
+def openFile():
+    global file
+    file = askopenfilename(defaultextension=".txt",
+                           filetypes=[("All Files", "*.*"),
+                                     ("Text Documents", "*.txt")])
+    if file == "":
+        file = None
